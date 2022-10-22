1f2e70741241087aea723961c0410950e3f75c15
isaias <104476799+mrza1127@users.noreply.github.com>
2022-10-21 23:28:01 -0500

    post table has been seeded

---
+++ seeds/post-seeds.js
@@ -0,0 +1,29 @@
+const {Post} = require('../models')
+
+const PostData = [
+    {
+        title: 'What is a 400 error?',
+        post_entry: '400 is a bad request. The server cannot comprehend what the user is trying to get',
+        user_id: 1
+    },
+    {
+        title: 'What is a Hook?',
+        post_entry: 'Hooks are functions that are to be called before or after another function is to be executed.',
+        user_id: 2
+    },
+    {
+        title: 'What is a third part API?',
+        post_entry: 'An API is a: application programming interface.',
+        user_id: 3
+    },
+    {
+        title: 'Do I need sessions?',
+        post_entry: 'Preferrably',
+        user_id: 4
+    },
+    {
+        title: 'Is the console real?',
+        post_entry: 'Yes',
+        user_id: 5
+    },
+]
