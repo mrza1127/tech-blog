6f6d4303dea34972190e0a24af1b5e7140bb9339
isaias <104476799+mrza1127@users.noreply.github.com>
2022-10-21 23:19:10 -0500

    created post model/new packages

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, "", {
   host: '127.0.0.1',
   dialect: 'mysql',
-  port: 3001,
+  port: 3306,
 });
 
 module.exports = sequelize;
--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ class User extends Model {
     }
   }
 
-  //colums for User model
+  //columns for User model
   User.init({
     id: {
         type: DataTypes.INTEGER,
