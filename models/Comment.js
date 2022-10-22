2188ac3a656483d19c56d2e14650277339b8c293
isaias <104476799+mrza1127@users.noreply.github.com>
2022-10-21 23:20:36 -0500

    associations

---
+++ models/Comment.js
@@ -0,0 +1,53 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create Comment model
+class Comment extends Model {}
+
+
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+
+            }
+        },
+
+        comment_entry: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }
+)
+
+
+module.exports = Comment;
