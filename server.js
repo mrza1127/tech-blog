5bb35b731adc592a41d67d21710e0547f9a89e8f
isaias <104476799+mrza1127@users.noreply.github.com>
2022-10-21 23:13:06 -0500

    added files

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const sequelize = require("./config/connection");
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
